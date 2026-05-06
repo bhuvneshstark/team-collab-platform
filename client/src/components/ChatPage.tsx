5da1ec0b3a87d86dab9cb57be9ad5a18663ad114
bhuvneshstark <82388579+bhuvneshstark@users.noreply.github.com>
2026-05-06 12:09:36 +0530

    Remove unused useCallback import

--- client/src/components/ChatPage.tsx
+++ client/src/components/ChatPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef, useCallback } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { io, Socket } from 'socket.io-client';
 import { auth } from '../config/firebase';
 import api from '../services/api';
